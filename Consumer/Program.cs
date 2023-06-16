34ebc98c5a878389b1c0d828524d03b97db5b5f9
Helder Guerreiro <helder.guerreiro@distinctepisode.com>
2023-06-16 13:06:53 +0100

    Initial Commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,53 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "test-consumer-group",
+    BootstrapServers = "localhost:9092",
+    //SslCaLocation = "/PathTO/cluster-ca-certificate.pem",
+    //SecurityProtocol = SecurityProtocol.SaslSsl,
+    //SaslMechanism = SaslMechanism.ScramSha256,
+    //SaslUsername = "ickafka",
+    //SaslPassword = "yourpassword",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+Console.WriteLine("Preparing connection");
+
+using (var c = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    c.Subscribe("test-topic");
+
+    CancellationTokenSource cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, e) =>
+    {
+        e.Cancel = true; // prevent the process from terminating.
+        cts.Cancel();
+    };
+
+    Console.WriteLine("triggering reads.");
+
+    try
+    {
+        while (true)
+        {
+            try
+            {
+                var cr = c.Consume(cts.Token);
+                c.Commit();
+                Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine($"Error occurred: {e.Error.Reason}");
+
+            }
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        Console.WriteLine("finnishing.");
+        // Ensure the consumer leaves the group cleanly and final offsets are committed.
+        c.Close();
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+ 
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"//,
+    //SslCaLocation = "/Path-to/cluster-ca-certificate.pem",
+    //SecurityProtocol = SecurityProtocol.SaslSsl,
+    //SaslMechanism = SaslMechanism.ScramSha256,
+    //SaslUsername = "ickafka",
+    //SaslPassword = "yourpassword",
+
+};
+
+using (var p = new ProducerBuilder<Null, string>(config).Build())
+{
+    try
+    {
+        do
+        {
+            string value = await Task.Run(() =>
+            {
+                Console.WriteLine("Enter text (or quit to exit)");
+                Console.Write("> ");
+                return Console.ReadLine();
+            });
+
+            if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                break;
+
+            var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = value });
+            Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+        } while (true);
+    }
+    catch (ProduceException<Null, string> e)
+    {
+        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+    }
+}
